544ff2a1152eeaaae556734751eecad55a87d27a
tonga54 <grodriguez160597@gmail.com>
2020-01-07 17:38:25 -0300

    Cambio mensaje login

--- usuarios/views.py
+++ usuarios/views.py
@@ -48,10 +48,9 @@ def login(request):
     email = request.data.get("email")
     password = request.data.get("password")
     if email is None or password is None:
-        return Response({'error': 'Please provide both username and password'}, status=HTTP_400_BAD_REQUEST)
-    # user = authenticate(email=email, password=password)
+        return Response({'error': 'Por favor provenga de email y password.'}, status=status.HTTP_400_BAD_REQUEST)
     user = EmailBackend().authenticate(username = email, password = password)
     if not user:
-        return Response({'error': 'Invalid Credentials'}, status=HTTP_404_NOT_FOUND)
+        return Response({'error': 'Invalid Credentials'}, status=status.HTTP_404_NOT_FOUND)
     token, _ = Token.objects.get_or_create(user=user)
-    return Response({'token': token.key, 'user' : user.email}, status=HTTP_200_OK)
+    return Response({'token': token.key, 'user' : user.email}, status=status.HTTP_200_OK)
